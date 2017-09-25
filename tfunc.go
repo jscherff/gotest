f8c5ebb194f2eaeb45f4de183d0205a8e7316036
John Scherff <jscherff@gmail.com>
2017-09-24 18:35:32 -0700

    Moved HereInfo() function to gotest repo

--- tfunc.go
+++ tfunc.go
@@ -1,6 +1,8 @@
 // See article https://medium.com/@benbjohnson/structuring-tests-in-go-46ddee7a25c
 // Code from https://github.com/benbjohnson/testing
 
+package gotest
+
 import (
 	"fmt"
 	"path/filepath"
@@ -9,30 +11,55 @@ import (
 	"testing"
 )
 
-// assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+// Assert fails the test if the condition is false.
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
+		tb.Fatalf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
 	}
 }
 
-// ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
+// Ok fails the test if an err is not nil.
+func Ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
+		tb.Fatalf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 	}
 }
 
-// equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
+// Equals fails the test if exp is not equal to act.
+func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
+		tb.Fatalf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
 	}
 }
 
+// NotEquals fails the test if exp is not equal to act.
+func NotEquals(tb testing.TB, exp, act interface{}) {
+	if reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		tb.Fatalf("\033[31m%s:%d:\n\n\texp: NOT %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+	}
+}
+
+
+// HereInfo prints useful information about an execution point
+func HereInfo() {
+
+	// Need space only for PC of caller.
+	pc := make([]uintptr, 1)
+
+	// Skip PC of Callers and decorator function.
+	n := runtime.Callers(2, pc)
+
+	// Return if no PCs available.
+	if n == 0 { return }
+
+	// Obtain the caller frame.
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+
+	// Print file and line number, then return.
+	fmt.Printf("%s:%d\n", frame.File, frame.Line)
+}
